1a230aca9cfeee4d1e370c27fdf8af26a9ce1166
vicentepese <sempereperisvicente@gmail.com>
2019-12-03 13:12:31 -0800

    final version

--- uniqueSeqsPTM.py
+++ uniqueSeqsPTM.py
@@ -44,9 +44,12 @@ def getRandomColor(options, PTM_count):
 	color = {PTM: ['<span style=\"background: '+'#%02X%02X%02X; font-weight: bold' % (r(),r(),r()) + '\">',"</span>"] for PTM in list(PTM_count.keys())}
 	color['mutation'] = ['<span style=\"border-style: solid;  border-width: 2px\">','</span>']
 	color['mutmatch'] = ['; border-style: solid; border-width: 2px']
-	color['red'] = ['<span style=\"background: red; font-weight: bold\"', '</span>']
+	color['red'] = ['<span style=\"background: red; font-weight: bold; \">', '</span>']
 
 	return color
+
+def div0(n, d):
+	return n / d if d and n else 0
 
 def mapSeqPTM(data, refprot, options):
 
@@ -69,7 +72,7 @@ def mapSeqPTM(data, refprot, options):
 			seqCount[AAseq][seq[3]] += 1
 			seqInit[AAseq] = int(seq[2])
 
-			# If sequence has PTM, save instances and indexes
+			# If sequence has PTM, save instances and indexes, else save sequence
 			if '[' in seq[1][2:-2]:
 				PTM_instances = re.findall('\[(.*?)\]', seq[1][2:-2], re.DOTALL)
 				PTM_idx = re.finditer('\[(.*?)\]', seq[1][2:-2], re.DOTALL)
@@ -87,27 +90,36 @@ def seq2HTML(options, seqPTM, seqCount, seqInit, PTM_count, refProt):
 
 	# Initialize 
 	init_pos = min(seqInit[seq] for seq in list(seqInit.keys()))
-	seqHTML = list()
+	seqHTML = defaultdict(str)
 	Markdowner = Markdown()
+
 	# Get random colors
 	color = getRandomColor(options, PTM_count)
 
 	# For each sequence
 	for seq in list(seqPTM.keys()):
+
 		seqMark = seq
 		PTM_pos_loop = list(seqPTM[seq].keys())
-		for pos in PTM_pos_loop:
-			for ptm in list(seqPTM[seq][pos].keys()):
-				seqMark = seqMark[0:pos] + color['red'][0] + seqMark[pos] + color['red'][1] + seqMark[(pos+1):]
-			PTM_pos_loop = [pos + len(color[ptm][0]) + len(color[ptm][1]) for pos in PTM_pos_loop]
+		PTM_pos_loop.sort()
+		for i in range(0,len(PTM_pos_loop)):
+			seqMark = seqMark[0:PTM_pos_loop[i]] + color['red'][0] + seqMark[PTM_pos_loop[i]] + color['red'][1] + seqMark[(PTM_pos_loop[i]+1):]
+			PTM_pos_loop = [pos + len(color['red'][0]) + len(color['red'][1]) for pos in PTM_pos_loop]
 
 		# Append initial location and ARP and PAN proportion 
 		seq_init_pos = seqInit[seq]
 		seqMark = '&nbsp;'*(np.absolute(init_pos-seq_init_pos)) + seqMark 
-		for pos in list(seqPTM[seq].keys()):
+		seqPos = list(seqPTM[seq].keys())
+		seqPos.sort()
+		for pos in seqPos:
+			seqMark = seqMark + ' // ' + '__' + str(pos) + '__: '
 			for ptm in list(seqPTM[seq][pos].keys()):
-				seqMark = seqMark + '//' + '__' + str(pos) + '__: ' + color[ptm][0] + ptm + color[ptm][1] + ' \(ARP:{:.2%}' ' PAN:{:.2%}\)'.format(seqPTM[seq][pos][ptm]['ARP']/PTM_count['ARP'], seqPTM[seq][pos][ptm]['PAN']/PTM_count['PAN'])
-		seqHTML.append(Markdowner.convert(seqMark + '\n'))
+				 seqMark = seqMark + color[ptm][0] + ptm + color[ptm][1] + ' \(ARP:{:.2%}' ' PAN:{:.2%}\)'.format(div0(seqPTM[seq][pos][ptm]['ARP'],seqCount[seq]['ARP']), div0(seqPTM[seq][pos][ptm]['PAN'],seqCount[seq]['PAN']))
+
+		seqHTML[seq] = Markdowner.convert(seqMark + '\n')
+	
+	# Sort initial position
+	seqInit = OrderedDict([(k, seqInit[k]) for k in sorted(seqInit, key=seqInit.get)])
 
 	# String for protein of reference 
 	pos_range = options['pos_range']
@@ -115,12 +127,23 @@ def seq2HTML(options, seqPTM, seqCount, seqInit, PTM_count, refProt):
 	refProt_string = '&nbsp;'*(pos_range[0] - init_pos - 4) + str(pos_range[0]) +'.' + ''.join(refProt_string)
 
 
+
 	# Save 	
-	with open('test.html','w') as outFile:
-		outFile.write('<style>' + '\n' + 'p { \n' + 'line-height:0.1; \n' + 'font-family: "Courier New", Courier, monospace; \n' + '} \n' + '\n' +"</style>" +'\n')
-		outFile.write(Markdowner.convert(refProt_string + '\n'))
-		outFile.write(Markdowner.convert('&nbsp; \n'))
-		outFile.writelines(seqHTML)
+	with open(options['html']["scroll-template"], 'r') as inFile:	
+		with open(options['files']['uniqueSeqsPTM.html'],'w') as outFile:
+			for line in inFile:
+				outFile.write(line)
+			refProt_string = Markdowner.convert(refProt_string + '\n')
+			refProt_string = refProt_string
+			outFile.write(Markdowner.convert(refProt_string + '\n'))
+			outFile.write(Markdowner.convert('&nbsp; \n'))
+			for seq in seqInit.keys():
+				if seqHTML[seq]:
+					seq_string = seqHTML[seq]
+					outFile.write(seq_string)
+				else:
+					continue
+			
 
 def main():
 
