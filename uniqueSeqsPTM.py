e7f613327085b55c5a98eae71efda1ce2a9f280b
vicentepese <sempereperisvicente@gmail.com>
2019-12-17 13:33:35 -0800

    simplify to import utils

--- uniqueSeqsPTM.py
+++ uniqueSeqsPTM.py
@@ -1,89 +1,14 @@
 import numpy as np 
-import sys 
-import os 
-import json 
-import csv 
 import re
-import random 
-import subprocess
+import json
 from markdown2 import Markdown
 from Bio import Entrez
 from Bio import SeqIO
 from collections import defaultdict, OrderedDict
 from scipy import stats
+from utils import getBindingCore, importBindData,\
+	 importData, reference_retreive, div0, getBindingCore, getRandomColor
 
-def importData(options):
-
-	# Import data  as list of lists
-	data = list() 
-	with open(options['files']['mergedFiltMASS'], 'r') as inFile:
-		reader = csv.reader(inFile)
-		for row in reader:
-			data.append(row)
-	
-	return data
-
-def reference_retreive(proteinID):
-
-	# Retrieve protein and get dictionnary of each peptide position
-	Entrez.email = "ambati@stanford.edu"
-	handle = Entrez.efetch(db="protein", rettype="fasta", retmode="text", id=proteinID)
-	seq_record = SeqIO.read(handle, "fasta")
-
-	# For each reference
-	out_ref={}
-	for i, j in enumerate(str(seq_record.seq)):
-		out_ref[i+1] =j
-	return out_ref
-
-def importBindData(options):
-
-	# Import binders 
-	binders = defaultdict(int)
-	with open(options['files']['DQ0602Binders'],'r') as inFile:
-		next(inFile)
-		reader = csv.reader(inFile)
-		for row in reader:
-			binders[row[2]] = float(row[3])
-	
-	# Import binding cores 
-	bindingCores = defaultdict(str)
-	with open(options['files']['DQ0602BindingCores'], 'r') as inFile:
-		next(inFile)
-		reader = csv.reader(inFile, delimiter = '\t')
-		for row in reader:
-			bindingCores[row[4]] = row[6]
-
-	return binders, bindingCores
-
-def getRandomColor(options, PTM_count):
-
-	# Create color dictionnary	
-	r = lambda: random.randint(75,200)
-	color = {PTM: ['<span style=\"background: '+'#%02X%02X%02X; font-weight: bold' % (r(),r(),r()) + '\">',"</span>"] for PTM in list(PTM_count.keys())}
-	color['red'] = ['<span style=\"background: red; font-weight: bold; \">', '</span>']
-	color['orange'] = ['<span style=\"background: orange; font-weight: bold; \">', '</span>']
-	color['bindingCore'] = ['<span style=\"background: green; font-weight: bold; \">', '</span>']
-
-	return color
-
-def div0(n, d):
-	return n / d if d and n else 0
-
-def countPositions(data):
-
-	# Position count 
-	posCount = defaultdict(lambda: defaultdict(int))
-
-	# Count number of occurences of an AA for each position 
-	for seq in data:
-		
-		# Clear data and count AA 
-		AAseq = re.sub('\[.+?\]','', seq[1][2:-2])
-		for i in range(0,len(AAseq)):
-			posCount[int(seq[2])+i][AAseq[i]] += 1 
-
-	return posCount
 
 def statisticalTest(options, PTM_seq, seqCount, seq):
 
@@ -110,31 +35,10 @@ def statisticalTest(options, PTM_seq, seqCount, seq):
 				if pvalue < 0.05:
 					sig_pval.append(pos)
 
-
 	return PTM_stats, sig_pval
 
-def getBindingCore(options, refProt):
-
-	# Import groundtruth and binding core 
-	binders, bindingCores = importBindData(options)
-
-	# Create array protein reference
-	refProtStr = ''.join([refProt[AA] for AA in list(refProt.keys())])
-
-	# Take binders with less than 10% of affinity, find the binding core,
-	# and find indexes in protein of reference 
-	strongBinders = list()
-	coreIdxs = list()
-	for binder in binders:
-		if float(binders[binder]) <= 10 and binder in refProtStr:
-			strongBinders.append(binder)
-			core = bindingCores[binder]
-			idx = re.search(core, refProtStr)
-			coreIdxs.append(idx.span())
-	
-	return coreIdxs
-
 def checkOverlap(seq_init_pos, seq_end_pos, coreIdxs):
+	
 	# If there is a partial overlap of at least one AA
 	for core in coreIdxs:
 		if any([idx in range(seq_init_pos, seq_end_pos) for idx in core]):
@@ -184,7 +88,7 @@ def seq2HTML(options, seqPTM, seqCount, seqInit, PTM_count, refProt, coreIdxs):
 	Markdowner = Markdown()
 
 	# Get random colors
-	color = getRandomColor(options, PTM_count)
+	color = getRandomColor(options, PTM_count=PTM_count)
 
 	# For each sequence
 	for seq in list(seqPTM.keys()):
