204ebf21a17e1ef851d3c88d5d6736ba11274458
vicentepese <sempereperisvicente@gmail.com>
2020-01-17 11:07:11 -0800

    Scenario A&B added

--- testGlycosylation.py
+++ testGlycosylation.py
@@ -9,32 +9,103 @@ from utils import getBindingCore, importBindData,\
 	 importData, reference_retreive, getBindingCore
 
 
-def getGlyco(options, data):
+def getGlycoAmid(options, data):
 
     # Initialize 
-    glycoCount = defaultdict(int)
     vaccSample = defaultdict(int)
+    glycoCount = defaultdict(int)
+    nonglycoCount = defaultdict(int)
+    nonglycoSample = defaultdict(int)
+    nonDeamidCount = defaultdict(int)
+    nonDeamidSample = defaultdict(int)
+    nonGlDa = defaultdict(int)
+    nonGlDaSample = defaultdict(int)
+    glycoRange = options['glycoRange']
 
     # Count the number of glycosylation
     for seq in data:
 
         # Initialize sequence
         AAseq = seq[1][2:-2]
+        AAnonPTM = re.sub('\[.+?\]', '', AAseq)
         initPos = int(seq[2])
-        endPos = initPos + len(AAseq)
+        endPos = initPos + len(AAnonPTM) -1
         pos_range = options['pos_range']
 
-        # If segement within defined range
+        # If segement overlaps with defined range
         if not(initPos > pos_range[1]) and not(endPos < pos_range[0]): 
-
-            # If sequence is glycosylated, count
-            if '+203.079' in AAseq:
-                glycoCount[seq[3]] += 1
-
+            
             # Count sample 
             vaccSample[seq[3]] +=1
 
-    return glycoCount, vaccSample
+            # Count if glycosylated 
+            if '+203.079' in AAseq:
+                glycoCount[seq[3]] += 1
+            
+            ########### SCENARIO B ###########
+            # If sequence contains both 277N and glycosylation sites
+            if not(initPos >= glycoRange[0] ) and not(endPos <= glycoRange[1]) and 277 in range(initPos, endPos+1):
+                
+                # Find indexes and instances of PTMs
+                PTM_idx = re.finditer('\[(.*?)\]', AAseq, re.DOTALL)
+                PTM_instances = re.findall('\[(.*?)\]', AAseq, re.DOTALL)
+
+                # Initialize variables
+                idx_cumm = 0
+                flag = 0
+                NPos = 277 - initPos
+
+                # For each instance, if there is a glycosylation within the range, raise a flag
+                for instance, idx in zip(PTM_instances, PTM_idx):
+                    if instance == '+203.079' and idx.start() -1 -idx_cumm + initPos in range(glycoRange[0], glycoRange[1]+1):
+                        flag = 1
+                    idx_cumm += len(instance) + 2
+                
+                # If there is a deamidatio in 277N, raise a flag
+                if not(AAseq[NPos+2:NPos+8] == '+0.984'):
+                    flag = 1
+
+                # If flag was not raised, then the sequence did not contain neither deamidation nor glycosylation. Count. 
+                if flag == 0:
+                    nonGlDa[seq[3]] += 1
+                
+                # Count the sample of sequences that contained both 277N and glycosylation sites
+                nonGlDaSample[seq[3]] +=1
+
+            ########### SCENARIO A ###########
+
+            # If the sequence contains glycsylation site
+            if not(initPos >= glycoRange[0] ) and not(endPos <= glycoRange[1]):
+
+                # Find indexes and instances of PTMs
+                PTM_idx = re.finditer('\[(.*?)\]', AAseq, re.DOTALL)
+                PTM_instances = re.findall('\[(.*?)\]', AAseq, re.DOTALL)
+
+                # Initialize variables
+                idx_cumm = 0
+                flag = 0
+                NPos = 277 - initPos
+
+                # For each instance, check glycosylation and raise flag
+                for instance, idx in zip(PTM_instances, PTM_idx):
+                    if instance == '+203.079' and idx.start() -1 -idx_cumm + initPos in range(286, 288+1):
+                        flag = 1
+                    idx_cumm += len(instance) + 2
+                
+                # If flag not raised (no glyco.) count
+                if flag == 0:
+                    nonglycoCount[seq[3]] += 1  
+
+                # Count sample
+                nonglycoSample[seq[3]] += 1                          
+                    
+            # Count non-amidated 277N
+            if 277 in range(initPos, endPos+1):
+                if not(AAseq[NPos+2:NPos+8] == '+0.984'):
+                    nonDeamidCount[seq[3]] += 1
+                nonDeamidSample[seq[3]] += 1
+
+    return vaccSample, glycoCount, nonglycoCount, nonglycoSample, nonDeamidCount, nonDeamidSample, nonGlDa, nonGlDaSample
 
 def fisherTest(glycoCount, vaccSample):
 
@@ -68,19 +139,59 @@ def main():
     # Import data 
     data = importData(options)
 
-    # Count the number of glycosylation within the segment
-    glycoCount, vaccsample = getGlyco(options, data)
+    # Get glycosylation count, and non-glycosylated sites and non-amidated 277N 
+    vaccSample, glycoCount, nonglycoCount, nonglycoSample \
+        , nonDeamidCount, nonDeamidSample, nonGlDa, nonGlDaSample = getGlycoAmid(options, data)
 
     # Compute fisher test
-    fisherResult = fisherTest(glycoCount, vaccsample)
+    fisherResult = fisherTest(glycoCount, vaccSample)
 
     # Print 
+    print('\n')
+    print('Glycosylation Fisher\'s test result:')
     for vacc in fisherResult:
-        print(vacc + ' {:.2%} ({})'.format(glycoCount[vacc]/vaccsample[vacc], vaccsample[vacc]) + \
+        print(vacc + ' {:.2%} ({})'.format(glycoCount[vacc]/vaccSample[vacc], vaccSample[vacc]) + \
             ' vs. PAN {:.2%} ({}): pvalue = {:.2}, oddsratio = {:.2}'.format( \
-            glycoCount['PAN']/vaccsample['PAN'], vaccsample['PAN'], \
-            fisherResult[vacc]['pvalue'], fisherResult[vacc]['oddsratio']) + \
-                 '\n')
+            glycoCount['PAN']/vaccSample['PAN'], vaccSample['PAN'], \
+            fisherResult[vacc]['pvalue'], fisherResult[vacc]['oddsratio']))
+
+    # Print probabilities of deamidation or glycosylation as separate entities which can 
+    # apper in different sequences. This method does not allow to compute a Fisher's test, but 
+    print('\n')
+    print('SCENARIO A: Considering 277N and glycosylation sites as different entities that can occur in separate segments')
+    print('Probability of non-deamydated and non-glycosylated in ARP:' + \
+         '{:.2%}'.format((nonglycoCount['ARP']/nonglycoSample['ARP']) * (nonDeamidCount['ARP']/nonDeamidSample['ARP'])))
+    print('Probability of non-deamydated and non-glycosylated in FOC:' + \
+         '{:.2%}'.format((nonglycoCount['FOC']/nonglycoSample['FOC']) * (nonDeamidCount['FOC']/nonDeamidSample['FOC'])))     
+    print('Probability of non-deamydated and non-glycosylated in PAN:' + \
+         '{:.2%}'.format((nonglycoCount['PAN']/nonglycoSample['PAN']) * (nonDeamidCount['PAN']/nonDeamidSample['PAN'])))
+
+    # Print probabilities of sequences that include both 277N and glycosylation sites. 
+    # This allows to compute Fisher's test PAN vs ARP and FOC.  
+    print('\n')
+    print('SCENARIO B: Considering only sequence that included both 277N and glycosylation sites:')
+    print('Probability of non-deamydated and non-glycosylated in ARP:' + \
+          '{:.2%}'.format((nonGlDa['ARP']/nonGlDaSample['ARP'])))
+    print('Probability of non-deamydated and non-glycosylated in FOC:' + \
+          '{:.2%}'.format((nonGlDa['FOC']/nonGlDaSample['FOC'])))
+    print('Probability of non-deamydated and non-glycosylated in PAN:' + \
+          '{:.2%}'.format((nonGlDa['PAN']/nonGlDaSample['PAN'])))
+
+     # Fisher test on SCENARIO B:
+    nonGlDaFisher = fisherTest(nonGlDa, nonGlDaSample)
+
+    # Print 
+    print('\n')
+    for vacc in nonGlDaFisher:
+        print('Fisher\'s test result for SCENARIO B:')
+        print(vacc + ' {:.2%} ({})'.format(nonGlDa[vacc]/nonGlDaSample[vacc], nonGlDaSample[vacc]) + \
+            ' vs. PAN {:.2%} ({}): pvalue = {:.2}, oddsratio = {:.2}'.format( \
+            nonGlDa['PAN']/nonGlDaSample['PAN'], nonGlDaSample['PAN'], \
+            fisherResult[vacc]['pvalue'], fisherResult[vacc]['oddsratio']))
+
+
+
+        
 
 if __name__ == "__main__":
     main()
